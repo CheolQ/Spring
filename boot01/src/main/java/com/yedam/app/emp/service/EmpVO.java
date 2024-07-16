202d51bf874a6ede362593b185b4c42a8c3c8023
CheolQ <rlacjfrb197@naver.com>
2024-07-16 17:51:40 +0900

    0716

--- boot01/src/main/java/com/yedam/app/emp/service/EmpVO.java
+++ boot01/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -12,7 +12,7 @@ public class EmpVO {
 	private String empname;
 	private int mgr;
 	private double sal;
-	private int deptid;
+	private Integer deptid;
 	@DateTimeFormat(pattern="yyyy-MM-dd") //	파라미터 날짜입력 자동변환 , JAVA는 대문자M 소문자m 구분하기
 	private Date hiredate; //	디폴트값 : 2024/07/11
 }
--- boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
+++ boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
@@ -45,12 +45,16 @@ public class EmpController {
 		EmpVO findVO = empService.empInfo(empVO);
 		model.addAttribute("empInfo", findVO);
 		return "emp/info";
+		//classpath:/templates/emp/info.html
 	}
 	
 	// 등록 - 페이지
 	@GetMapping("empInsert")
 	public String empInsertForm() {
+//		model.addAttribute("empVO", new EmpVO());
+		
 		return "emp/insert";
+		//classpath:/templates/emp/insert.html
 	}
 	
 	// 등록 - 처리(연산, submit)
@@ -82,14 +86,14 @@ public class EmpController {
 	}
 	
 	// 수정 - 처리(연산, AJAX => QueryString)
-	@PostMapping("empUpdate")
+//	@PostMapping("empUpdate")
 	@ResponseBody // => AJAX
 	public Map<String , Object> empUpdateAJAXQueryString(EmpVO empVO){
 		return empService.empUpdate(empVO);
 	}
 	
 	// 수정 - 처리(연산, AJAX => JSON : @RequestBody)
-//	@PostMapping("empUpdate")
+	@PostMapping("empUpdate")
 	@ResponseBody // => AJAX
 	public Map<String , Object> empUpdateAJAXJSON(@RequestBody EmpVO empVO){
 		return empService.empUpdate(empVO);
